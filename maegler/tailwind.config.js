4391852dff749445d51774466f1e80f1f1150fa0
KristineAlbers <1259@rts-365.dk>
2023-05-10 13:09:40 +0200

    add app layout

---
+++ maegler/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      
+        colors: {
+          cblue: {
+            900: '#162A41',
+          },
+        }
+      
+    },
+  },
+  plugins: [],
+}
+
